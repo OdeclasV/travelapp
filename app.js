ade47b0ea2954daaafc36bdcd66996da4f7fe0b3
Victor Salcedo <victorsalc2@gmail.com>
2016-11-06 08:02:34 -0500

    update files

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
+//Adding dependencies
 var express = require('express')
 var bodyParser = require('body-parser')
 var handlebrs = require('express-handlebars')
 
+//Connecting to my DB using Mongoose
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/traveler')
 
+//Adding my model 
 var Traveler = require('./models/traveler.js')
 
-
+//Initializing my app
 var app = express()
 
 
-
+//Using template engine
 app.engine('handlebars', handlebrs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + "/views")
 
+//Connecting with css file (main.css)
+app.use(express.static(__dirname + '/public'))
+
+//Using Body Parser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:true }))
 
  
 app.get('/', function( req, res ){
 
-	res.render('home', {'name': 'handlebars'});
+	res.render('home')
 })
 
 app.get('/person_info', function(req,res){
